acb8fb4e957e61b8408054fa295090c0b820921e
Sampo Sivonen <sampo.sivonen@helsinki.fi>
2018-09-22 13:07:43 +0300

    Tehtävä 1.5

--- Tehtävät/osa1/task1/src/index.js
+++ Tehtävät/osa1/task1/src/index.js
@@ -3,28 +3,31 @@ import ReactDOM from 'react-dom';
 
 
 const App = () => {
-    const kurssi = "Half Stack -sovelluskehitys"
+    const kurssi = {
+        nimi: "Half Stack -sovelluskehitys",
+        osat: [
+            {
+                nimi: "Reactin perusteet",
+                tehtavia: 10
+            },
+            {
+                nimi: "Tiedonvälitys propseilla",
+                tehtavia: 7
+            },
+            {
+                nimi: "Komponenttien tila",
+                tehtavia: 14
+            }
+        ]
 
-    const osat = [
-        {
-            nimi: "Reactin perusteet",
-            tehtavia: 10
-        },
-        {
-            nimi: "Tiedonvälitys propseilla",
-            tehtavia: 7
-        },
-        {
-            nimi: "Komponenttien tila",
-            tehtavia: 14
-        }
-    ]
+
+    }
 
     return (
         <div>
-            <Otsikko kurssi={kurssi} />
-            <Sisalto osat={osat} />
-            <Yhteensa osat={osat} />
+            <Otsikko kurssi={kurssi.nimi} />
+            <Sisalto osat={kurssi.osat} />
+            <Yhteensa osat={kurssi.osat} />
         </div>
     )
 }
